3660cc2de63eb5bd9a11414a5dbb78f49c52b124
SM imran <shariarimran94@gmail.com>
2025-03-21 23:10:48 +0600

    Add files via upload

--- problem6.c
+++ problem6.c
@@ -1,1 +1,20 @@
 
+//ET251075
+#include <stdio.h>
+#include <math.h>
+
+int main() {
+    float radius, area;
+
+    printf("Enter the radius of the circle: ");
+    scanf("%f", &radius);
+
+
+    area = M_PI * radius * radius;  // M_PI is a constant in math.
+
+
+    printf("The radius of the circle is: %.2f\n", radius);
+    printf("The area of the circle is: %.2f\n", area);
+
+    return 0;
+}
